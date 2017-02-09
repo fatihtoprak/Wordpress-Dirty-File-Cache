efaee731780775cf90a0af3003d96abb1b6b39a9
trgino <trgino@trgino.com>
2017-02-09 19:10:39 +0300

    Cache Clear File
    Only Access Logged In

--- clear.php
+++ clear.php
@@ -1,2 +1,36 @@
 <?php
 // cache clear file
+ini_set('memory_limit','1024M');
+function wp_login_check(){
+	$cacheLoggedIn = false;
+	if (count($_COOKIE)>0 && !empty($_COOKIE)) {
+		foreach ($_COOKIE as $key => $val) {
+			if (preg_match("/wordpress_logged_in/i", $key)) {
+				$cacheLoggedIn = true;
+			}
+		}
+	} else {
+		$cacheLoggedIn = false;
+	}
+	return $cacheLoggedIn;
+}
+if($cacheLoggedIn){
+	if(isset($_POST['delete'])){
+		$filez = glob('./cache/*.'.$_SERVER['SERVER_NAME']);
+		$count = count($filez);
+		if($count>0 && !empty($filez)){
+			foreach($filez as $f){
+				unlink($f);
+			}
+		}
+		echo $count.' file deleted';
+		echo '<script>setTimeout(function(){ window.history.back(); },3500); </script>';
+	}else{
+		echo '
+			<h1>'.$_SERVER['SERVER_NAME'].' Dirty File Cache</h1>;
+			<form methodt="post">
+			<input type="button" name="delete" value="Clear Cache">
+			</form>
+		';
+	}
+}
